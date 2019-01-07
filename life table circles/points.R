c9b7b0513260c2ff4b14fe4e56d8cf00e4f20322
Carl Schmertmann <schmertmann@fsu.edu>
2019-01-07 17:13:20 -0500

    getting there

--- life table circles/points.R
+++ life table circles/points.R
@@ -10,31 +10,38 @@ windows(record=TRUE)
 
 D = fread('USA SWE male 2016 life tables.txt') %>%
        as.data.frame() %>%
-       select(country, age, lx) 
+       select(country, age, lx)
 
 D$age   = 0:110    # make ages numeric
-D$radix = 100000   # add radix column
-D$x = D$y = 0
 
-## reorganize (wide) 
-D = D %>% 
-  spread(key=country, value=lx)
+D = rbind(D,
+         expand.grid(country='radix', 
+                     age=0:110, 
+                     lx=100000))
 
-G = ggplot(data=D,aes(x=x,y=y)) +
+
+D$x = 0
+D$y = 0
+
+
+D = D %>%
+     arrange(age,country)
+
+G = ggplot(data=D,aes(x=x,y=y, group=country, alpha=country,color=country)) +
         lims(x=c(-320,320), y=c(-320,320)) +
-        geom_point(size=sqrt(D$radix)/2, shape=1,color='black') +
-        geom_point(size=sqrt(D$SWE)/2, shape=16,color='gold', alpha=.30) +
-        geom_point(size=sqrt(D$USA)/2, shape=16,color='red',  alpha=.10) +
+        geom_point(size=sqrt(D$lx)/2, shape=16) +
         coord_fixed() +
-        labs(title=paste0('Survivors to age ',D$age,'\n[Outer Circle = 100,000 male births]'),
+        labs(title=paste0('Survivors to age {frame_time} \n[Outer Circle = 100,000 male births]'),
              caption='Source: Human Mortality Database http://mortality.org\n2016 mortality rates',
              fill = 'Country') +
         theme_no_axes() +
-        scale_fill_manual(values=c('gold','red' )) +
-        transition_states(age)
+        scale_color_manual(values=c(grey(.90),'gold','red')) +
+        scale_alpha_manual(values=c(NA,.30,.10)) +
+        transition_time(age)
 
 movie = animate(G, fps=4)
 
 anim_save('compare-life-tables-as-circles.gif', 
           animation=movie )
+
 

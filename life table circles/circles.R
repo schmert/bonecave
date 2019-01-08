7a50e2b44c8f57f0da78c4436096f3b67055a58e
Carl Schmertmann <schmertmann@fsu.edu>
2019-01-08 17:54:12 -0500

    still will not animate

--- life table circles/circles.R
+++ life table circles/circles.R
@@ -8,37 +8,43 @@ library(gganimate)
 graphics.off()
 windows(record=TRUE)
 
+# D = fread('USA SWE male 2016 life tables.txt') %>%
+#        as.data.frame() %>%
+#        select(country, age, lx) 
+# 
+# D$age   = 0:110    # make ages numeric
+# D$radix = 100000   # add radix column
+# 
+# ## reorganize (wide) 
+# D = D %>% 
+#   spread(key=country, value=lx)
+# 
+
 D = fread('USA SWE male 2016 life tables.txt') %>%
-       as.data.frame() %>%
-       select(country, age, lx) 
+  as.data.frame() %>%
+  select(country, age, lx)
 
 D$age   = 0:110    # make ages numeric
-D$radix = 100000   # add radix column
 
-## reorganize (wide) 
-D = D %>% 
-  spread(key=country, value=lx)
+D = rbind(D,
+          expand.grid(country='radix', 
+                      age=0:110, 
+                      lx=100000)) %>%
+          arrange(age,country)
 
-G = ggplot( ) +
-        geom_circle(
-            data=D,lwd=1,lty=1,
-            aes(x0=0, y0=0, r=sqrt(D$radix), fill=NULL),color='black'
-            ) +
-        geom_circle(
-          data=D,
-          aes(x0=0, y0=0, r=sqrt(D$SWE), fill='SWE' ), color='gold', alpha=.30
-        ) +  
-        geom_circle(
-          data=D,
-          aes(x0=0, y0=0, r=sqrt(D$USA), fill='USA'), color='red', alpha=.10
-        ) +  
+
+G = ggplot(data=D,aes(group=country, 
+                      color=country, 
+                      fill =country,
+                      alpha=country )) +
+        geom_circle(aes(x0=0, y0=0, r=sqrt(lx) ),lwd=1,lty=1) +
         coord_fixed() +
         labs(title=paste0('Survivors to age ',D$age,'\n[Outer Circle = 100,000 male births]'),
              caption='Source: Human Mortality Database http://mortality.org\n2016 mortality rates',
              fill = 'Country') +
         theme_no_axes() +
-        scale_fill_manual(values=c('gold','red' )) +
-        transition_time(age)
+        scale_fill_manual(values=c('gold','red','blue' )) +
+        transition_reveal(age)
 
 movie = animate(G, fps=4)
 

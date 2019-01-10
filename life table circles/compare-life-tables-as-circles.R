df8f953472d3bb4605f365be325927147390678a
Carl Schmertmann <schmertmann@fsu.edu>
2019-01-10 11:08:23 -0500

    phase 1 complete

--- life table circles/compare-life-tables-as-circles.R
+++ life table circles/compare-life-tables-as-circles.R
@@ -1,9 +1,8 @@
 # devtools::install_github('thomasp85/gganimate')
 library(tidyverse)
-library(ggforce)
 library(data.table)
 library(gganimate)
-
+library(grid)
 
 graphics.off()
 windows(record=TRUE)
@@ -32,21 +31,25 @@ D = rbind(D,
                       lx=100000)) %>%
           arrange(age,country)
 
+fpa = 4  # frames per age
+aps = 3  # ages per second
 
-G = ggplot(data=D,aes(group=country, 
-                      color=country, 
-                      fill =country,
-                      alpha=country )) +
-        geom_circle(aes(x0=0, y0=0, r=sqrt(lx) ),lwd=1,lty=1) +
+G = ggplot(data=D,aes(x=0, y=0)) +
+        geom_point(aes(size=lx, color=country, alpha=country)) +
+        scale_size_area(guide = FALSE, max_size = 175) +
         coord_fixed() +
-        labs(title=paste0('Survivors to age ',D$age,'\n[Outer Circle = 100,000 male births]'),
+        labs(title=paste0('Survivors to age {(frame-1) %/% fpa}'),
+             subtitle='[Outer Circle = 100,000 male births]',
              caption='Source: Human Mortality Database http://mortality.org\n2016 mortality rates',
              fill = 'Country') +
         theme_no_axes() +
-        scale_fill_manual(values=c('gold','red','blue' )) +
-        transition_reveal(age)
+        scale_color_manual(name='',values=c('lightgrey','gold','red')) +
+        scale_alpha_manual(values=c(.30,.50,.25)) +
+        guides(alpha=FALSE, 
+               color = guide_legend(override.aes = list(size=10))) +
+        transition_time(age) 
 
-movie = animate(G, fps=4)
+movie = animate(G, nframes=111*fpa, fps=aps*fpa)
 
 anim_save('compare-life-tables-as-circles.gif', 
           animation=movie )

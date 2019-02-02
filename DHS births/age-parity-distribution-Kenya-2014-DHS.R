8fdb1bbdd91065eebab236daf604bcf795b67f64
Carl Schmertmann <schmertmann@fsu.edu>
2019-02-02 18:43:53 -0500

    corrected new plot

--- DHS births/age-parity-distribution-Kenya-2014-DHS.R
+++ DHS births/age-parity-distribution-Kenya-2014-DHS.R
@@ -16,6 +16,7 @@ ddi    = read_ipums_ddi("idhs_00003.xml")
 Women  = read_ipums_micro(ddi)
 
 counts = Women %>% 
+           filter(COUNTRY==404) %>%
            group_by(AGE,CHEB) %>% 
            summarize(n=sum(PERWEIGHT)) %>%
            rename(age=AGE, parity=CHEB)
@@ -29,7 +30,7 @@ png(file='age-parity-distribution-Kenya-2014-DHS.png')
        geom_point(shape=16, color='royalblue', alpha=.50) +
        scale_size_continuous(range=c(0,15)) +
        scale_y_continuous(breaks=seq(15,49,2)) +
-       scale_x_continuous(breaks=0:max(history$parity)) +
+       scale_x_continuous(breaks=0:max(counts$parity)) +
        labs(title='Distribution of Women by Age and Parity',
             subtitle='Kenya 2014 DHS',
             x='# children ever born',

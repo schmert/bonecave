08c7646da2357d0b99e84a4bfcac6242b6f86c8d
Carl Schmertmann <schmertmann@fsu.edu>
2019-04-23 10:39:01 -0400

    big speedup
    no more eigen-decompostition.  Solve QP directly with sparse matrices

--- poly-to-hex-mapping/fast-hex-smoothing.R
+++ poly-to-hex-mapping/fast-hex-smoothing.R
@@ -3,28 +3,38 @@ library(rgdal)
 library(sf)
 library(sp)
 library(spdep)
+library(Matrix)
 
 rm(list=ls())
 graphics.off()
 
 windows(record=TRUE)
 
-# get geographical information from schmert.net
-# make a df with microregion numbers and names
-file_url = 'http://topals-mortality.schmert.net/data/muni.df.csv'
+this_state_abb  = c('AL','SE','BA','PI','MA','CE','RN','PB','PE')
+this_state_long = c('ALAGOAS','SERGIPE','BAHIA','PIAUI','MARANHAO','CEARA','RIO GRANDE DO NORTE','PARAIBA','PERNAMBUCO')
 
-this_state_abb  = 'BA'
-this_state_long = 'BAHIA'
+nhex = 2000
+  
+########################################################
+# get the geographical information and e0 estimates if necessary
 
-geo = read.csv(file_url) %>% 
-        filter(ufabb==this_state_abb)  %>%
-        select(ufabb,munid, municode, muniname, microcode, microname) 
+if (!file.exists('muni.df.csv')) {
+  file_url = 'http://topals-mortality.schmert.net/data/muni.df.csv'
+  download.file(url = file_url, destfile = 'muni.df.csv', mode='wb')
+}
 
+if (!file.exists('e0_microregion_summary.csv')) {
+  file_url = 'http://mortality-subregistration.schmert.net/data/e0_microregion_summary.csv'
+  download.file(url = file_url, destfile = 'e0_microregion_summary.csv', mode='wb')
+}
 
-# get state e0 estimates from schmert.net
-file_url = 'http://mortality-subregistration.schmert.net/data/e0_microregion_summary.csv'
+# filter out selected geography
 
-e = read.csv(file_url) %>%
+geo = read.csv('muni.df.csv') %>% 
+          filter(ufabb %in% this_state_abb)  %>%
+          select(ufabb,munid, municode, muniname, microcode, microname) 
+
+e = read.csv('e0_microregion_summary.csv') %>%
       filter(coverage_model=='partial',
              pctile==50, 
              sex=='m', 
@@ -50,10 +60,10 @@ for (suffix in c('.shp','.dbf','.shx')) {
 
 ## municipio map
 municipality_map = st_read(dsn='.', layer='brasilmun2010_new') %>%
-       filter(NAME2_ == this_state_long) %>%
+       filter(NAME2_ %in% this_state_long) %>%
        select('ID_', contains('NAME'), geometry) %>%
        mutate(municode = floor( as.numeric( as.character(ID_)) / 10)) %>%
-       full_join(geo)
+       left_join(geo)
 
 plot(municipality_map['e0'])
 
@@ -72,14 +82,14 @@ plot(microregion_map['e0'])
 
 state_boundary = municipality_map %>%
                    group_by(NAME2_) %>%
-                   summarize(state=this_state_abb)
+                   summarize()
                    
-plot(state_boundary['state'])                   
+plot(state_boundary)                   
 
 # generate a grid of hexagons that covers the state
 hex_points = spsample(as_Spatial(state_boundary),
                       type='hexagonal',
-                      n=2000)
+                      n=nhex)
 
 hexagons = HexPoints2SpatialPolygons(hex_points) %>%
             st_as_sf()
@@ -88,31 +98,35 @@ H = nrow(hexagons)
 
 plot(hexagons, col='red', border='white')
 
-## peek at overlaps
-tmp = st_intersection(hexagons, microregion_map)
-plot(tmp['e0'], border='white')
+## examine overlaps
+overlap_map = st_intersection(hexagons, microregion_map)
+
+A = st_area(overlap_map)  # areas of overlapping (polygon,hexagon) pairs
+
+plot(overlap_map['e0'], border='white')
 
 #-- speedup experiment here
 
-A = st_area(tmp)
+## find the overlaps between microregion polygons and
+## hexagons: W is a (#polygons x #hexagons) = PxH matrix
+## with row sums=1, such that 
+## W %*% [hexagon values] = [polygon values]
 
 # candidates will be a matrix of (microregion #, hexagon #) combinations
 # that have some overlap. In most cases the hexagon will be entirely inside
 # the microregion polygon
+
 Imat = as.matrix(st_intersects(microregion_map, hexagons))
 candidates = which(Imat, arr.ind = TRUE)
 colnames(candidates) = c('microregion','hexagon')
-
-head(candidates)
 
 W = matrix(0, P, H)
 W[candidates] = A
 W = prop.table(W,1)
 
-
 ###############################
 
-v = microregion_map$e0
+polygon_values = microregion_map$e0
 
 ## make quadratic penalty matrix for the hexagons,
 ## based on adjacencies
@@ -123,29 +137,29 @@ diag(K) = sapply(adj, length)
 
 for (i in seq(adj)) { K[i, adj[[i]]] = -1 }
 
-## find the overlaps between microregion polygons and
-## hexagons: W is a (#polygons x #hexagons) = PxH matrix
-## with row sums=1, such that 
-## W %*% [hexagon values] = [polygon values]
 
-## SPARSE MATRIX STUFF
+## use sparse matrices to solve the quadratic programming problem:
+##    min z'Kz st Wz = polygon_values
+## where z'Kz is the sum of sq diffs between adjacent hex values
+##
+## This will find the smoothest hexmap that exactly matches the
+## polygon values when averaged by area
 
-library(Matrix)
 A = Matrix( rbind( cbind( 2*K, -t(W)), 
                    cbind(   W, diag(0,P)) ) )
-b = Matrix( c(rep(0,H),v), ncol=1)
+b = Matrix( c(rep(0,H), polygon_values), ncol=1)
 
-vhex = (solve(A,b))[1:H]
+optimal_hex_values = (solve(A,b))[1:H]
 
-hexagons$e0 = vhex
+hexagons$e0 = optimal_hex_values
+
+## plot the smoothest hex map
 
 ggplot( data=hexagons, aes(fill=e0)) +
   geom_sf(color=NA) +
   scale_fill_viridis_c(option='C') +
   labs(title=this_state_long)  +
   theme_minimal()
-
-
 
 
 

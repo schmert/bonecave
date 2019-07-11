2c290697928ff003044495bab00e3ecb54c05892
Carl Schmertmann <schmertmann@fsu.edu>
2019-07-11 18:39:17 -0400

    added Mexico 2015

---
+++ Mexico-2015-fertility-by-HH-income/TFR by income.R
@@ -0,0 +1,34 @@
+# NOTE: To load data, you must download both the extract's data and the DDI
+# and also set the working directory to the folder with these files (or change the path below).
+
+library(tidyverse)
+
+## converting from indiv -> households is EXTREMELY slow
+## if it's already been done, don't repeat
+
+data.already.processed = TRUE
+
+if (!data.already.processed) {
+  if (!require("ipumsr")) stop("Reading IPUMS data into R requires the ipumsr package. It can be installed using the following command: install.packages('ipumsr')")
+  
+  ddi  <- read_ipums_ddi("ipumsi_00015.xml")
+  data <- read_ipums_micro(ddi) 
+  
+  # ## Household-level data with total earned income and C,W counts
+  # MX = data %>%
+  #        head(500000) %>%
+  #        group_by(SERIAL) %>%
+  #        summarize(HHincome = sum(INCEARN[INCEARN < 99999998]),
+  #                  C        = sum( AGE %in% 0:4),
+  #                  W15      = sum( (SEX==2 & (AGE %in% 15:19))),
+  #                  W20      = sum( (SEX==2 & (AGE %in% 20:24))),
+  #                  W25      = sum( (SEX==2 & (AGE %in% 25:29))),
+  #                  W30      = sum( (SEX==2 & (AGE %in% 30:34))),
+  #                  W35      = sum( (SEX==2 & (AGE %in% 35:39))),
+  #                  W40      = sum( (SEX==2 & (AGE %in% 40:44))),
+  #                  W45      = sum( (SEX==2 & (AGE %in% 45:49))),
+  #                  HHWT     = HHWT[1]
+  #                  )
+  
+} # !data.already.processed
+

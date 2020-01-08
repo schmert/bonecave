b52114bc5b0927b98d6fa21b8bb6b78344f5b6de
Carl Schmertmann <schmertmann@fsu.edu>
2020-01-08 11:44:38 -0500

    started animation

--- UN-WPP/age-structure-animation.R
+++ UN-WPP/age-structure-animation.R
@@ -5,6 +5,8 @@
 
 library(wpp2019)
 library(tidyverse)
+library(gganimate)
+library(gifski)
 
 data(pop)
 data(popproj)
@@ -14,23 +16,29 @@ oage = 60
 xlab = paste0('Fraction ',oage,"+")
 ylab = paste0('Fraction Under ',yage)
 
-
-sel_countries = c('Asia',
-                  'Northern America',
-                  'Europe', 
-                  'South America',
-                  'Africa')
+# keep the names in alphabetical order to avoid color mismatches
+sel = tribble(
+ ~name, ~txt, ~color,
+ 'Africa','Africa','darkgreen',
+ 'Asia', 'Asia', 'blue',
+ 'Europe','Europe','red',
+ 'Latin America and the Caribbean', 'Lat Am/Caribb', 'purple',
+ 'Northern America', 'N. America', 'orange'
+) 
+#%>%
+#  mutate(name=factor(name))
+               
 
 # observed populations up through 2020 ----
 
 FF = popF %>%
        select(-country_code) %>%
-       filter(name %in% sel_countries) %>% 
+       filter(name %in% sel$name) %>% 
        gather(key='period', value='Fpop', -name, -age)
 
 MM = popM %>%
   select(-country_code) %>%
-  filter(name %in% sel_countries) %>% 
+  filter(name %in% sel$name) %>% 
   gather(key='period', value='Mpop', -name, -age)
 
 big_obs = full_join(FF,MM) %>%
@@ -42,26 +50,17 @@ big_obs = full_join(FF,MM) %>%
                      elder = sum(pop[x >= oage])/total)
 
 
-ggplot(data=big_obs) +
-  aes(x=elder, y=young, color=name,group=name) +
-  geom_point(aes(size=total), alpha=.50) +
-  geom_line(size=0.2) +
-  labs(x=xlab,y=ylab, 
-       title='Age Structure',
-       caption='Source: UN World Population Prospects 2019') +
-  theme_bw()
 
-
-# forecasts through 2100 ----
+# median forecasts through 2100 ----
 
 FF = popFprojMed %>%
   select(-country_code) %>%
-  filter(name %in% sel_countries) %>% 
+  filter(name %in% sel$name) %>% 
   gather(key='period', value='Fpop', -name, -age)
 
 MM = popMprojMed %>%
   select(-country_code) %>%
-  filter(name %in% sel_countries) %>% 
+  filter(name %in% sel$name) %>% 
   gather(key='period', value='Mpop', -name, -age)
 
 big_pred = full_join(FF,MM) %>%
@@ -72,24 +71,32 @@ big_pred = full_join(FF,MM) %>%
             young = sum(pop[x <  yage])/total,
             elder = sum(pop[x >= oage])/total)
 
+big = bind_rows(big_obs,
+                big_pred) %>%
+      mutate(period = as.integer(period)) %>%
+      left_join(sel, by='name')
 
-ggplot(data=big_pred) +
-  aes(x=elder, y=young, color=name,group=name) +
-  geom_point(aes(size=total), alpha=.50) +
-  geom_line(size=0.2) +
-  labs(x=xlab,y=ylab, 
-       title='Age Structure',
-       caption='Source: UN World Population Prospects 2019') +
-  theme_bw()
+A = ggplot(data=big) +
+      aes(x=elder, y=young, color=name, group=name, label=txt) +
+      geom_point(aes(size=total), alpha=.70) +
+      scale_size_continuous(range=c(0,30)) +
+      scale_color_manual(values=sel$color) +
+      geom_text(nudge_y=-.04, size=5) +
+      guides(size=FALSE,color=FALSE) +
+      labs(x=xlab,y=ylab,
+           title='xx',
+           caption='Source: UN World Population Prospects 2019') +
+      geom_line(size=0.2) +
+      theme_bw() +
+      transition_reveal(period)
 
+animate(
+  A,
+  fps = 5,# 24,
+  duration = 30,
+  width = 500,
+  height = 500,
+  renderer = gifski_renderer('age-structure.gif')
+)
 
-ggplot(data=bind_rows(big_obs,big_pred)) +
-  aes(x=elder, y=young, color=name,group=name) +
-  geom_point(aes(size=total), alpha=.50) +
-  scale_size_continuous(range=c(0,15)) +
-  labs(x=xlab,y=ylab, 
-       title='Age Structure',
-       caption='Source: UN World Population Prospects 2019') +
-  geom_line(size=0.2) +
-  theme_bw()
-
+    

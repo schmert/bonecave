21df70411ad523bc60b61447e1318d932dce3f92
Carl Schmertmann <schmertmann@fsu.edu>
2021-01-15 13:40:28 -0500

    first draft of pyramid

--- demography-US-congressional-districts/population-pyramids-by-party.R
+++ demography-US-congressional-districts/population-pyramids-by-party.R
@@ -1,14 +1,16 @@
 library(tidyverse)
 library(tidycensus)
 
+rm(list = ls())
+
 ## legislative info
 
-D = read_csv('legislators-current.csv') %>% 
+legis = read_csv('legislators-current.csv') %>% 
      filter(type=='rep', state %in% state.abb) %>% 
      select(gender:district,party) %>% 
      rename(dist_num = district)
 
-head(D)
+head(legis)
 
 ## census info by district
 # get API key
@@ -24,46 +26,9 @@ fips = fips_codes %>%
   select(state, state_name, state_code) %>% 
   mutate(state_num = as.numeric(state_code))
 
-# the small data frame below is a crosswalk to
-# identify a 5-year age group for any of the 
-# (sometimes more disaggregated) groups that 
-# appear in the table for total populations
-age_group_info = tribble(
-  ~age_label, ~group,
-  'Under 5 years',     '0-4',
-  '5 to 9 years' ,     '5-9',
-  '10 to 14 years',  '10-14',
-  '15 to 17 years',  '15-19',
-  '18 and 19 years', '15-19',
-  '20 years',        '20-24',
-  '21 years',        '20-24',
-  '22 to 24 years',  '20-24',
-  '25 to 29 years',  '25-29',
-  '30 to 34 years',  '30-34',
-  '35 to 39 years',  '35-39',
-  '40 to 44 years',  '40-44',
-  '45 to 49 years',  '45-49',
-  '50 to 54 years',  '50-54',
-  '55 to 59 years',  '55-59',
-  '60 and 61 years', '60-64',
-  '62 to 64 years',  '60-64',
-  '65 and 66 years', '65-69',
-  '67 to 69 years',  '65-69',
-  '70 to 74 years',  '70-74',
-  '75 to 79 years',  '75-79',
-  '80 to 84 years',  '80-84',
-  '85 years and over', '85+'
-)
+# the small data frames below are crosswalks 
+# that give details for ACS variable names
 
-# convert group to a properly-ordered factor
-L = seq(0,80,5)
-
-age_group_info = age_group_info %>% 
-                   mutate(group = factor(group, 
-                          levels=c(paste0(L,'-',L+4),'85+')))
-
-$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-  
 Table1_info = tribble(
   ~variable, ~label, ~L, ~H, ~sex, ~race,
 'B01001_003',	'Under 5 years' , 0 , 4, 'male', 'all',
@@ -89,6 +54,7 @@ Table1_info = tribble(
 'B01001_023',	'75 to 79 years' ,75,79,'male', 'all',
 'B01001_024',	'80 to 84 years' ,80,84,'male', 'all',
 'B01001_025',	'85 years and over',85,Inf,'male', 'all',
+
 'B01001_027',	'Under 5 years' , 0 , 4, 'female', 'all',
 'B01001_028',	'5 to 9 years'  , 5,  9, 'female', 'all',
 'B01001_029',	'10 to 14 years' ,10, 14, 'female', 'all',
@@ -147,154 +113,181 @@ TableH_info = tribble(
 'B01001H_031', '85 years and over',  85, Inf, 'female', 'WNH'
 )
 
-'B01001H_01', 'Under 5 years' , 
-'B01001H_01', '5 to 9 years' , 
-'B01001H_02', '10 to 14 years' , 
-'B01001H_02', '15 to 17 years' , 
-'B01001H_02', '18 and 19 years' , 
-'B01001H_02', '20 to 24 years' , 
-'B01001H_02', '25 to 29 years' , 
-'B01001H_02', '30 to 34 years' , 
-'B01001H_02', '35 to 44 years' , 
-'B01001H_02', '45 to 54 years' , 
-'B01001H_02', '55 to 64 years' , 
-'B01001H_02', '65 to 74 years' , 
-'B01001H_03', '75 to 84 years' , 
-'B01001H_03', '85 years and over'
-)
 
-'$$$$$$$'$$'$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
-'MALE -all
-'01001_003'Under 5 years
-'B01001_004'	5 to 9 years
-'B01001_005'10 to 14 years
-'B01001_006'	15 to 17 years
-'B01001_007'18 and 19 years
-'B01001_008'	20 years
-'B01001_009'21 years
-'B01001_010'	22 to 24 years
-'B01001_011'25 to 29 years
-'B01001_012'	30 to 34 years
-'B01001_013'35 to 39 years
-'B01001_014'	40 to 44 years
-'B01001_015'45 to 49 years
-'B01001_016'	50 to 54 years
-'B01001_017'55 to 59 years
-'B01001_018'	60 and 61 years
-'B01001_019'62 to 64 years
-'B01001_020'	65 and 66 years
-'B01001_021'67 to 69 years
-'B01001_022'	70 to 74 years
-'B01001_023'75 to 79 years
-'B01001_024'	80 to 84 years
-'B01001_025'85 years and over
-''
-'FEMALE 'al'
-'B01001_027'Under 5 years
-'B01001_028'	5 to 9 years
-'B01001_029'10 to 14 years
-'B01001_030'	15 to 17 years
-'B01001_031'18 and 19 years
-'B01001_032'	20 years
-'B01001_033'21 years
-'B01001_034'	22 to 24 years
-'B01001_035'25 to 29 years
-'B01001_036'	30 to 34 years
-'B01001_037'35 to 39 years
-'B01001_038'	40 to 44 years
-'B01001_039'45 to 49 years
-'B01001_040'	50 to 54 years
-'B01001_041'55 to 59 years
-'B01001_042'	60 and 61 years
-'B01001_043'62 to 64 years
-'B01001_044'	65 and 66 years
-'B01001_045'67 to 69 years
-'B01001_046'	70 to 74 years
-'B01001_047'75 to 79 years
-'B01001_048'	80 to 84 years
-'B01001_049'85 years and over
-# 
-# white-alone NON-HIST versions of these are in table B01001H_... NOT QUITE, B
-#  BECAUSE THE $%$%$ AGE GROUPS ARE DIFFT IN THE RACE-SPECIFIC TABLES
-
-# MALE
-# B01001H_003	Under 5 years 
-# B01001H_004	5 to 9 years 
-# B01001H_005	10 to 14 years 
-# B01001H_006	15 to 17 years 
-# B01001H_007	18 and 19 years 
-# B01001H_008	20 to 24 years 
-# B01001H_009	25 to 29 years 
-# B01001H_010	30 to 34 years 
-# B01001H_011	35 to 44 years 
-# B01001H_012	45 to 54 years 
-# B01001H_013	55 to 64 years 
-# B01001H_014	65 to 74 years 
-# B01001H_015	75 to 84 years 
-# B01001H_016	85 years and over 
-# 
-# FEMALE
-# B01001H_018	Under 5 years 
-# B01001H_019	5 to 9 years 
-# B01001H_020	10 to 14 years 
-# B01001H_021	15 to 17 years 
-# B01001H_022	18 and 19 years 
-# B01001H_023	20 to 24 years 
-# B01001H_024	25 to 29 years 
-# B01001H_025	30 to 34 years 
-# B01001H_026	35 to 44 years 
-# B01001H_027	45 to 54 years 
-# B01001H_028	55 to 64 years 
-# B01001H_029	65 to 74 years 
-# B01001H_030	75 to 84 years 
-# B01001H_031	85 years and over 
-
-
-
-
-Table1_age_labels = c('Under 5 years', '5 to 9 years', '10 to 14 years',
-'15 to 17 years', '18 and 19 years', '20 years', '21 years', '22 to 24 years',
-'25 to 29 years', '30 to 34 years', '35 to 39 years', '40 to 44 years',
-'45 to 49 years', '50 to 54 years', '55 to 59 years', '60 and 61 years',
-'62 to 64 years', '65 and 66 years', '67 to 69 years', '70 to 74 years',
-'75 to 79 years', '80 to 84 years', '85 years and over')
-
- 
-TableH_age_labels = c('Under 5 years', '5 to 9 years', '10 to 14 years', 
-'15 to 17 years', '18 and 19 years', '20 to 24 years', '25 to 29 years', 
-'30 to 34 years', '35 to 44 years', '45 to 54 years', '55 to 64 years', 
-'65 to 74 years', '75 to 84 years', '85 years and over')
-
-vars = c( paste0('B01001_00',3:9),  paste0('B01001_0',10:25) ,paste0('B01001_0',27:49), 
-          paste0('B01001H_00',3:9), paste0('B01001H_0',10:16),paste0('B01001A_0',18:31))
-
-
-names(vars) = c( paste0('Total-Male-',Table1_age_labels), 
-                 paste0('Total-Female-',Table1_age_labels),
-                 paste0('White-Male-',TableH_age_labels), 
-                 paste0('White-Female-',TableH_age_labels) ) 
-
-
-P = get_acs(geography = 'congressional district',
-            variables = vars,
+# data for all races (more age detail)
+P1 = get_acs(geography = 'congressional district',
+            variables = Table1_info$variable,
             year=2019,
             output = 'tidy') %>% 
       mutate(state_num = as.numeric(substr(GEOID,1,2)),
              dist_num  = as.numeric(substr(GEOID,3,4)))   %>% 
       left_join(fips) %>% 
-      transform( age_label = c( Table1_age_labels, 
-                                 Table1_age_labels,
-                                 TableH_age_labels, 
-                                 TableH_age_labels)) %>% 
-     left_join(age_group_info)
+      left_join(Table1_info)
 
 
+## merge political info and aggregate
+## after this operation, rawpop is a 23x1 vector
+## for groups starting at ages
+##  0  5 10 15 18 20 21 22 25 30 35 40 45 50 55 60
+##  62 65 67 70 75 80 85
 
-%>% 
-  pivot_wider(id_cols     = GEOID,
-              names_from  = variable,
-              values_from = estimate) %>% 
+D1 = left_join(legis, P1, by=c('state','dist_num')) %>% 
+      select(-NAME,-moe,-state_num, -state_code) %>% 
+      group_by(party,L,H,sex,race) %>% 
+      summarize(pop=sum(estimate)) %>%
+      ungroup() %>% 
+      select(-L,-H) %>% 
+      nest(rawpop = pop) 
 
-## merge info and then aggregate by party
 
-D = left_join(D, P, by=c('state','dist_num'))
+# data for white non-Hispanics (less age detail)
+PH = get_acs(geography = 'congressional district',
+             variables = TableH_info$variable,
+             year=2019,
+             output = 'tidy') %>% 
+  mutate(state_num = as.numeric(substr(GEOID,1,2)),
+         dist_num  = as.numeric(substr(GEOID,3,4)))   %>% 
+  left_join(fips) %>% 
+  left_join(TableH_info)
+
+
+## merge political info and aggregate
+## after this operation, rawpop is a 14x1 vector
+## for groups starting at ages
+##   0  5 10 15 18 20 25 30 35 45 55 65 75 85
+
+DH = left_join(legis, PH, by=c('state','dist_num')) %>% 
+  select(-NAME,-moe,-state_num, -state_code) %>% 
+  group_by(party,L,H,sex,race) %>% 
+  summarize(pop=sum(estimate)) %>%
+  ungroup() %>% 
+  select(-L,-H) %>% 
+  nest(rawpop = pop) 
+
+######### convert both data sets to common 5-yr age groups
+#  18 groups: 0-4, 5-9, 10-14, ... , 80-84, 85+
+
+# table 1 had 23 age groups. Aggregate to 18
+
+### matrix W1 such that 
+###   W1 %*% [vector of 23 pops from Table 1] = [vector of 18 5-yr pop groups]
+
+Lstar = seq(0,85,5)
+L     = sort( unique(Table1_info$L))
+
+W1 = outer(Lstar, L, function(x,y) 1*(x<=y)*(y < x+5))
+dimnames(W1) = list(Lstar,L)
+
+sm5 = function(x) { as.vector(W1 %*% unlist(x))}
+
+D1 = D1 %>% 
+      mutate(smoothpop = lapply(rawpop, sm5))
+
+
+## function to find smoothest set of 
+## 18 5-yr pop totals that exactly match the 14
+## totals in Table H
+
+# N must be a 14-vector for groups with L as
+# in Table H: 0 5 10 15 18 20 25 30 35 45 55 65 75 85
+
+# first have to define a 13 x 14 matrix to aggregate the
+# 15-17 and 18-19 groups in the race-specific data
+# CURSE YOU, CENSUS BUREAU!
+
+L1 = c(seq(0,30,5), seq(35,85,10))
+H1 = c( tail(L1,-1), Inf)
+L2 = c(0, 5, 10, 15, 18, 20, 25, 30, 35, 45, 55, 65, 75, 85)
+H2 = c( tail(L2,-1), Inf)
+
+A =  outer(seq(L1),seq(L2), 
+    function(i,j) 1*(L1[i]<=L2[j])*(H1[i] >= H2[j]))
+dimnames(A) = list(L1,L2)
+
+# W is an 13 x 18 matrix to collapse the a hypothetical
+# pop disaggregated into 18 5-year groups into the
+# data 13 groups observed in the race-specific table
+# CURSE YOU, CENSUS BUREAU!
+
+Lstar = seq(0,85,5)
+Hstar = c( tail(Lstar,-1), Inf)
+
+W =  outer(seq(L1),seq(Lstar), 
+           function(i,j) 1*(L1[i]<=Lstar[j])*(H1[i] >= Hstar[j]))
+dimnames(W) = list(L1,Lstar)
+
+# 16 x 18 2nd-differencing matrix for 18 groups
+D2 = diff(diag(length(Lstar)), diff=2)
+
+# penalty for non-linearity in 5-year population totals
+P = crossprod(D2)
+
+# solve for smoothest set of 18 5-yr pops that
+# matches the Table H raw totals
+
+Z = rbind( cbind(P, -t(W)),
+           cbind(W, diag(0,13)))
+
+sm5H = function(rawpopH) {
+    y = A %*% unlist(rawpopH)  # now 13 groups
+    v = c(rep(0,18), y)
+    soln = solve( Z, v)
+    return( soln[1:18])
+}
+
+
+DH = DH %>% 
+  mutate(smoothpop = lapply(rawpop, sm5H))
+
+
+## FINALLY. (CURSE YOU, CENSUS BUREAU!)
+
+POP = rbind(D1, DH)
+
+
+###########################################
+# NOW PLOT PYRAMIDS
+###########################################
+
+# unnest and widen, to make plotting more convenient
+
+
+plot_data = POP %>% 
+              select(-rawpop) %>% 
+              unnest(smoothpop) %>% 
+              transform(L=Lstar) %>% 
+              mutate(smoothpop = ifelse(sex=='male',-1,+1)*smoothpop ) %>% 
+              pivot_wider(names_from=race,values_from =smoothpop ) 
+  
+# add a factor for age group
+age = factor(Lstar, 
+             levels=Lstar, 
+             labels= c(paste(head(Lstar,-1),head(Hstar,-1),sep='-'),
+                         '85+'))
+
+plot_data = plot_data %>% 
+             transform(age=age)
+
+
+## plot all
+
+G = ggplot(data=plot_data) +
+  aes(x = age, y=all) +
+  geom_bar(data=filter(plot_data,sex=='male'),
+           stat='identity', fill='cornflowerblue',alpha=.80, width=.90) +
+  geom_bar(data=filter(plot_data,sex=='female'),
+           stat='identity', fill='lightcoral', alpha=.80, width=.90) +
+  scale_y_continuous(breaks=seq(-6e6, 6e6, 2e6),
+                     labels=c('6M','4M','2M','0','2M','4M','6M'),
+                     minor_breaks = NULL) +
+  coord_flip() +
+  facet_grid(~party) +
+  theme_bw() 
+
+G + 
+  geom_bar(data=filter(plot_data,sex=='male'),aes(x=age,y=all-WNH,fill=sex),
+           stat='identity', alpha=1, fill='blue', width=.90) +
+  geom_bar(data=filter(plot_data,sex=='female'),aes(x=age,y=all-WNH,fill=sex),
+           stat='identity', alpha=1, fill='red', width=.90)
+
+  

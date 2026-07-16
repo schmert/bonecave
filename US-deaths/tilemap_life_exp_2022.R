306547207b061363b301838b29852d0526571402
Carl Schmertmann <schmertmann@fsu.edu>
2026-07-15 21:19:50 -0300

    Update tilemap_life_exp_2022.R

--- US-deaths/tilemap_life_exp_2022.R
+++ US-deaths/tilemap_life_exp_2022.R
@@ -10,27 +10,34 @@ abbs = sort( c(state.abb, 'DC'))
 df = tibble()
 
 for (this_abb in abbs) {
-  this_file  = paste0(this_abb,'_bltper_5x1.csv')
+  this_file  = paste0(this_abb,'_mltper_5x1.csv')
   this_fpath = paste0('States/',this_abb,'/',this_file)
   
   unzip(zipfile, files=this_fpath, junkpaths = TRUE)
   this_data = read_csv(this_file, show_col_types = FALSE) |> 
-               filter(Year==2022, Age=='65-69') |> 
+               filter(Year==2022, Age=='40-44') |> 
                select(Year,PopName,Age,ex) |> 
-               mutate(life_exp = factor(floor(ex)))
+               mutate(life_exp = factor(trunc(ex)))
   df = bind_rows(df, this_data)
   
   file.remove(this_file)
   print(this_abb)
 }
 
-df = df |> 
-      mutate(life_exp = factor(life_exp,levels=sort(unique(as.character(life_exp))))) |> 
+exvals = df$life_exp |> 
+          as.character() |> 
+          unique() |> 
+          as.numeric() |> 
+          sort()
+
+exlabs = paste(exvals,exvals+1, sep='-')
+
+  df = df |> 
+      mutate(life_exp = factor(life_exp,levels=exvals,labels=exlabs)) |> 
       left_join(us_state_grid1, by= join_by(PopName==code)) |> 
       mutate( x = col,
               y = max(row)-row+1)
 
-for (L in LETTERS[1:8]) {
   G = ggplot(data=df) +
     aes(x, y, fill = life_exp, label=PopName) +
     geom_tile(color='white') +
@@ -41,9 +48,10 @@ for (L in LETTERS[1:8]) {
           panel.border     = element_blank(),
           panel.background = element_blank(),
           axis.text = element_blank(),
-          axis.title = element_blank()) +
-    scale_fill_viridis_d(direction = -1, option=L ) +
-    labs(title=L)
+          axis.title = element_blank(),
+          plot.title = element_text(hjust=0.5)) +
+    scale_fill_viridis_d(direction = -1, option='D') +
+    labs(title='Remaining Male Life Expectancy\nat Age 65',
+         fill='Years')
   
   print(G)
-}

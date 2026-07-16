ce41ac6f63772b4d8970a72927e950414b709fef
Carl Schmertmann <schmertmann@fsu.edu>
2026-07-16 15:40:16 -0300

    convert age to numeric

--- US-deaths/extract-US-state-life-tables-5x1.R
+++ US-deaths/extract-US-state-life-tables-5x1.R
@@ -1,10 +1,26 @@
+#...........................................................
+#  reads the zip file of state-level life tables
+#  from the US Mortality Database
+#  
+#  Barbieri, M., and Winant, C. 
+#  “U.S State Life Tables CSV + TXT 1941-2022,” 2025. 
+#  https://doi.org/10.7910/DVN/19WYUX
+#  
+#  and extracts and writes three csv files 
+#  (m=male, f=female, b=both sexes combined)
+#  containing the state-level 5[age]x1[year] tables
+#...........................................................
+
 library('here')
 library('tidyverse')
+
 
 zipfile = 'USStateLifetables2022.zip'
 
 abbs = sort( c(state.abb, 'DC'))
 
+
+age = c(0,1,seq(from=5,to=110,by=5))
 
 for (sex in c('b','m','f')) {
 
@@ -17,8 +33,11 @@ for (sex in c('b','m','f')) {
     
     unzip(zipfile, files=this_fpath, junkpaths = TRUE)
     this_data = read_csv(this_file, show_col_types = FALSE) |> 
-                 select(Year,PopName,Age,mx:ex) |> 
-                 add_column(sex,.after='PopName')
+                 mutate(Sex=sex) |> 
+                 group_by(PopName,Year,Sex) |> 
+                 mutate(Age=age) |> 
+                 ungroup() |> 
+                 select(PopName,Year,Sex,Age,mx:ex) 
     
     df = bind_rows(df, this_data)
     
